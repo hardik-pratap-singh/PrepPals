1c0773db248f5cff16474478e719240daa41aad3
Hardik Sharma <hardik182002@gmail.com>
2024-04-06 02:04:27 +0530

    added readme

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,9 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  skills: [{ type: String }],
+  level: { type: String, required: true },
+  points: [{ type: String}],
+//   skills: [{ type: String }],
 });
 
 const User = mongoose.model('User', userSchema);
