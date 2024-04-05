955848f8f0f29c28493db0f277fdc8dc71e6ddd3
Aniket Shekhar Chaudhari <aniketsc93@gmail.com>
2024-04-06 01:45:04 +0530

    first me aniket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
+import {createBrowserRouter,
+  RouterProvider,} from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Landing from "./routes/Landing."
 // import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing/>,
+    },
+  ]);
 
   return (
     <>
-      PayPals
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ frontend/src/routes/Landing..jsx
@@ -0,0 +1,168 @@
+// import React,{useState} from 'react'
+
+
+// function Landing(){
+    
+//     return (
+//     <div>
+//      hehe
+//     </div>
+//     );
+// }
+
+
+// export default Landing;
+
+export default function Landing() {
+
+    const createButton = document.querySelector("#createroom");
+const videoCont = document.querySelector('.video-self');
+const codeCont = document.querySelector('#roomcode');
+const joinBut = document.querySelector('#joinroom');
+const mic = document.querySelector('#mic');
+const cam = document.querySelector('#webcam');
+
+let micAllowed = 1;
+let camAllowed = 1;
+
+let mediaConstraints = { video: true, audio: true };
+
+navigator.mediaDevices.getUserMedia(mediaConstraints)
+    .then(localstream => {
+        videoCont.srcObject = localstream;
+    })
+
+function uuidv4() {
+    return 'xxyxyxxyx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const createroomtext = 'Creating Room...';
+
+createButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    createButton.disabled = true;
+    createButton.innerHTML = 'Creating Room';
+    createButton.classList = 'createroom-clicked';
+
+    setInterval(() => {
+        if (createButton.innerHTML < createroomtext) {
+            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length + 1);
+        }
+        else {
+            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length - 3);
+        }
+    }, 500);
+
+    //const name = nameField.value;
+    location.href = `/room.html?room=${uuidv4()}`;
+});
+
+joinBut.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (codeCont.value.trim() == "") {
+        codeCont.classList.add('roomcode-error');
+        return;
+    }
+    const code = codeCont.value;
+    location.href = `/room.html?room=${code}`;
+})
+
+codeCont.addEventListener('change', (e) => {
+    e.preventDefault();
+    if (codeCont.value.trim() !== "") {
+        codeCont.classList.remove('roomcode-error');
+        return;
+    }
+})
+
+cam.addEventListener('click', () => {
+    if (camAllowed) {
+        mediaConstraints = { video: false, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "nodevice";
+        cam.innerHTML = `<i class="fas fa-video-slash"></i>`;
+        camAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: true, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "device";
+        cam.innerHTML = `<i class="fas fa-video"></i>`;
+        camAllowed = 1;
+    }
+})
+
+mic.addEventListener('click', () => {
+    if (micAllowed) {
+        mediaConstraints = { video: camAllowed ? true : false, audio: false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.classList = "nodevice";
+        mic.innerHTML = `<i class="fas fa-microphone-slash"></i>`;
+        micAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: camAllowed ? true : false, audio: true };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.innerHTML = `<i class="fas fa-microphone"></i>`;
+        mic.classList = "device";
+        micAllowed = 1;
+    }
+})
+
+
+
+
+  
+    return (
+      <div>
+
+
+<navbar>
+        <div className="logo">QuickMeet</div>
+    </navbar>
+
+
+
+        <div className="main">
+            <div className="create-join">
+                <div className="text"><div className="head">Create Video Meetings in one-click.</div>
+                <div className="subtext">No sign ups required. Open Source platform.</div>
+                </div>
+                <button id="createroom" className="createroom-butt unselectable">Create Room</button><br/>
+                <input type="text" name="room" spellCheck="false" placeholder="Enter Room Code" id="roomcode" className="roomcode"/><br/>
+                <div className="joinroom unselectable" id="joinroom">Join Room</div>
+            </div>
+            <div className="video-cont">
+                <video className="video-self" autoPlay muted playsinline></video>
+                <div className="settings">
+                    <div className="device" id="mic"><i className="fas fa-microphone"></i></div>
+                    <div className="device" id="webcam"><i className="fas fa-video"></i></div>
+                </div>
+            </div>
+        </div>
+        
+    
+
+
+      </div>
+    );
+  }
