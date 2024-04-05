053172ecb7e3c0e3c7a2ca11df27ef5ae2589e3d
Hardik Sharma <hardik182002@gmail.com>
2024-04-06 04:05:17 +0530

    added socket.io backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-require("dotenv").config()  ; 
+require("dotenv").config();
 const express = require("express")
-const app = express() ; 
+const socketio = require('socket.io');
+const moment = require('moment');
+const app = express();
 const db = require("./db");
-db() ; 
+db();
 
 const cors = require("cors");
 app.use(cors());
@@ -10,11 +12,113 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/" , (req , res) => {
+let rooms = {};
+let socketroom = {};
+let socketname = {};
+let micSocket = {};
+let videoSocket = {};
+let roomBoard = {};
+
+
+const server = app.listen(5000, () => {
+    console.log("server running on port 5000");
+})
+
+const io = socketio(server);
+
+
+io.on('connect', socket => {
+
+    socket.on("join room", (roomid, username) => {
+
+        socket.join(roomid);
+        socketroom[socket.id] = roomid;
+        socketname[socket.id] = username;
+        micSocket[socket.id] = 'on';
+        videoSocket[socket.id] = 'on';
+
+        if (rooms[roomid] && rooms[roomid].length > 0) {
+            rooms[roomid].push(socket.id);
+            socket.to(roomid).emit('message', `${username} joined the room.`, 'Bot', moment().format(
+                "h:mm a"
+            ));
+            io.to(socket.id).emit('join room', rooms[roomid].filter(pid => pid != socket.id), socketname, micSocket, videoSocket);
+        }
+        else {
+            rooms[roomid] = [socket.id];
+            io.to(socket.id).emit('join room', null, null, null, null);
+        }
+
+        io.to(roomid).emit('user count', rooms[roomid].length);
+
+    });
+
+    socket.on('action', msg => {
+        if (msg == 'mute')
+            micSocket[socket.id] = 'off';
+        else if (msg == 'unmute')
+            micSocket[socket.id] = 'on';
+        else if (msg == 'videoon')
+            videoSocket[socket.id] = 'on';
+        else if (msg == 'videooff')
+            videoSocket[socket.id] = 'off';
+
+        socket.to(socketroom[socket.id]).emit('action', msg, socket.id);
+    })
+
+    socket.on('video-offer', (offer, sid) => {
+        socket.to(sid).emit('video-offer', offer, socket.id, socketname[socket.id], micSocket[socket.id], videoSocket[socket.id]);
+    })
+
+    socket.on('video-answer', (answer, sid) => {
+        socket.to(sid).emit('video-answer', answer, socket.id);
+    })
+
+    socket.on('new icecandidate', (candidate, sid) => {
+        socket.to(sid).emit('new icecandidate', candidate, socket.id);
+    })
+
+    socket.on('message', (msg, username, roomid) => {
+        io.to(roomid).emit('message', msg, username, moment().format(
+            "h:mm a"
+        ));
+    })
+
+    socket.on('getCanvas', () => {
+        if (roomBoard[socketroom[socket.id]])
+            socket.emit('getCanvas', roomBoard[socketroom[socket.id]]);
+    });
+
+    socket.on('draw', (newx, newy, prevx, prevy, color, size) => {
+        socket.to(socketroom[socket.id]).emit('draw', newx, newy, prevx, prevy, color, size);
+    })
+
+    socket.on('clearBoard', () => {
+        socket.to(socketroom[socket.id]).emit('clearBoard');
+    });
+
+    socket.on('store canvas', url => {
+        roomBoard[socketroom[socket.id]] = url;
+    })
+
+    socket.on('disconnect', () => {
+        if (!socketroom[socket.id]) return;
+        socket.to(socketroom[socket.id]).emit('message', `${socketname[socket.id]} left the chat.`, `Bot`, moment().format(
+            "h:mm a"
+        ));
+        socket.to(socketroom[socket.id]).emit('remove peer', socket.id);
+        var index = rooms[socketroom[socket.id]].indexOf(socket.id);
+        rooms[socketroom[socket.id]].splice(index, 1);
+        io.to(socketroom[socket.id]).emit('user count', rooms[socketroom[socket.id]].length);
+        delete socketroom[socket.id];
+        console.log('--------------------');
+        console.log(rooms[socketroom[socket.id]]);
+
+        //toDo: push socket.id out of rooms
+    });
+})
+
+app.get("/", (req, res) => {
     res.send("Home Route");
 })
 
-
-app.listen(5000 , () => {
-    console.log("server running on port 5000");
-})
